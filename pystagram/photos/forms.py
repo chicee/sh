0319177b99bf958d3a9494cc376de44297c3ef2c
SungWook Shin <chic.sw@gmail.com>
2016-09-24 16:08:43 +0900

    profile추가

--- pystagram/photos/forms.py
+++ pystagram/photos/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.forms import ValidationError
+
 from .models import Post
 
 class PostSimpleForm(forms.Form):
@@ -10,3 +12,17 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['content', 'tags'] # 따로정의안되어있으면 관계있는거끌어옴
+
+    def clean_content(self):
+        content = self.cleaned_data['content']
+        if '바보' in content:
+            _msg = '본문에 금지어가 있습니다 : {}'.format('바보')
+            # self.add_error('content', _msg)
+            raise ValidationError(_msg) # 엄격하게 여기서 멈춤
+        return content # 주의! 반드시 정제된 결과를 리턴 해야함
+
+    def clean(self):
+        content = self.cleaned_data.get('content')
+        if content and '띨띨이' in content:
+            _msg = '방심하지마라! 금지어 있다: {}'.format('띨띨이')
+            self.add_error('content', _msg)
